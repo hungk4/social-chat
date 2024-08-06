6ce95db592514ccde149d7562eee7a26c249851b
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-08-06 18:00:27 +0700

    lam tinh nang chat co ban

--- public/js/script.js
+++ public/js/script.js
@@ -12,3 +12,20 @@ if(showAlert) {
   }, time);
 }
 // End show-alert
+
+// CLIENT_SEND_MESSAGE
+const formChat = document.querySelector(".chat .inner-form");
+if(formChat) {
+  formChat.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const content = event.target.content.value;
+    if(content) {
+      socket.emit("CLIENT_SEND_MESSAGE", {
+        content: content
+      });
+      event.target.content.value = "";
+    }
+  })
+}
+// End CLIENT_SEND_MESSAGE
